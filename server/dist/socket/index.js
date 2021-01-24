0eaf4be0f40f2d8755b17ca48201189670529ea4
ahsan8244 <syed.ahsan8244@gmail.com>
2021-01-24 20:08:07 +0800

    new build

--- server/dist/socket/index.js
+++ server/dist/socket/index.js
@@ -5,7 +5,12 @@ var handleSocket = function (socket) {
     socket.on("join_stream", function (streamId) {
         socket.join(streamId);
     });
+    socket.on("chat_message", function (_a) {
+        var streamId = _a.streamId, message = _a.message;
+        socket.to(streamId).emit("read_message", message);
+    });
     socket.on("start_stream", function (streamId) {
+        socket.join(streamId);
         socket.on("write_code", function (code) {
             console.log(code);
             socket.to(streamId).emit("read_code", code);
